7da5d02d212934d82e158ce1de1c88d3461bf35e
Frank <frank.delaguila@gmail.com>
2024-05-23 12:08:53 -0600

    Fixing resume link to new resume, and removing old online resume link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
-'use client';
-import type { ReactElement } from 'react';
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
-import Link from 'next/link';
+"use client";
+import type { ReactElement } from "react";
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import Link from "next/link";
 
-import Internal from '@/components/common/Internal';
-import { Button } from '@/components/ui/button';
-import Model from '@/components/common/Model';
+import Internal from "@/components/common/Internal";
+import { Button } from "@/components/ui/button";
+import Model from "@/components/common/Model";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faFileArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faFileArrowDown } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home(): ReactElement {
   return (
@@ -27,15 +27,16 @@ export default function Home(): ReactElement {
             Looking to hire a developer, or to get a custom website made?
           </p>
           <Button asChild className="font-bold mr-4 mb-4 min-[384px]:mb-0">
-            <Link
+            <a
+              href="./Frank_Delaguila_Resume_2024.pdf"
+              download="frankdelaguila.pdf"
               target="_blank"
               rel="noopener noreferrer"
-              href="https://bold.pro/my/frank-delaguila?vsid=18199fb7-7d38-44da-92bf-b6f2b37c5fd5"
             >
-              Online Resume
-            </Link>
+              Resume
+            </a>
           </Button>
-          <Button variant={'secondary'} asChild className="font-bold">
+          <Button variant={"secondary"} asChild className="font-bold">
             <Link href="/freelance">I need a website made!</Link>
           </Button>
           <div className="flex align-center">
@@ -47,7 +48,7 @@ export default function Home(): ReactElement {
               <FontAwesomeIcon
                 aria-label="My Github Profile"
                 title="My Github Profile"
-                className="cusor-pointer p-4 hover:text-primary transition-colors"
+                className="cusor-pointer pt-4 pb-4 pr-4 hover:text-primary transition-colors"
                 icon={faGithub}
                 size="2xl"
               />
@@ -65,20 +66,6 @@ export default function Home(): ReactElement {
                 size="2xl"
               />
             </Link>
-            <a
-              href="./Frank_Delaguila_Resume.pdf"
-              download="frankdelaguila.pdf"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon
-                aria-label="Download my resume!"
-                title="Frank Delaguilas Resume"
-                className="cusor-pointer p-4 hover:text-primary transition-colors"
-                icon={faFileArrowDown}
-                size="2xl"
-              />
-            </a>
           </div>
         </div>
         <div className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-black">
